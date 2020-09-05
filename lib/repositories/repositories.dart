6de12d9d5094af389a26d5f329b6d6d506475676
Augustine Victor <51150248+austinevick@users.noreply.github.com>
2020-09-05 10:23:20 +0100

    Update repositories.dart

--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -1,3 +1,5 @@
 export 'auth/base_auth_repository.dart';
 export 'auth/auth_repository.dart';
 export 'base_repositories.dart';
+export 'notes/notes_repository.dart';
+export 'notes/base_note_repository.dart';
